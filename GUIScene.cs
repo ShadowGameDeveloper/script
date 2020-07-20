4ad1cdbb0f2ba47d90edae92b0188403d40baf50
ShadowGameDeveloper <67132655+ShadowGameDeveloper@users.noreply.github.com>
2020-07-21 01:10:54 +0900

    Update GUIScene.cs

--- GUIScene.cs
+++ GUIScene.cs
@@ -22,6 +22,7 @@ public class GUIScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //canvasを隠します
         canvas.enabled = false;
         text.text = "Ready.....";
     }
@@ -29,6 +30,7 @@ public class GUIScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //スペースキー押せばcanvasを表示します
         if (Input.GetKey(KeyCode.Space))
         {
             canvas.enabled = true;
@@ -37,14 +39,17 @@ public class GUIScene : MonoBehaviour
 
     public void onButtonClick()
     {
+        //GUIボタン押せば
         text.text = "ok!!";
     }
     public void onToggleChange()
     {
+        //GUI　toggle　変わる時
         text.text = toggle.isOn ? "ON" : "off"; 
     }
     public void OnSliderChange()
     {
+        //GUI　slider　変わる時
         text.text = "value" + slider.value;
     }
 
@@ -56,14 +61,18 @@ public class GUIScene : MonoBehaviour
         {
             dx *= -1;
         }*/
+
+        //GUIボタン作ります
         /*if(GUI.Button(new Rect(100,100,100,25),"add count"))
         {
             this.count++;
         }*/
+        //GUItoggleボタン
         //this.flag = GUI.Toggle(new Rect(100, 100, 100, 25), this.flag, "Toggle");
         GUI.Box(new Rect(this.msg_x, 10, 350, 150), "Hello");
+        //GUI　slider
         //this.slid_value = GUI.HorizontalSlider(new Rect(100, 100, 100, 25), this.slid_value, 0, 100);
-
+        //GUI　text
         this.msg = GUI.TextField(new Rect(100, 100, 100, 25), this.msg);
         GUI.Label (new Rect(this.msg_x, 60, 300, 50), "「" + this.msg +"」と書きました。");
     }
