0eceef6886802c87ddbf7cf1b3ea4bf20f221bda
ShadowGameDeveloper <67132655+ShadowGameDeveloper@users.noreply.github.com>
2020-07-21 01:10:16 +0900

    Update State.cs

--- Game AI/Miner/State.cs
+++ Game AI/Miner/State.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public abstract class State<Entitype> where Entitype : BaseGameEntity
 {
+    //こちらは概要のクラス
     public abstract GameObject target
     {
         get;
     }
 
+    //ステート入ります
     public abstract void Enter(Entitype entity);
+    //ステートを実行します
     public abstract void Execute(Entitype entity);
+    //ステート出ます
     public abstract void Exit(Entitype entity);
+    //メッセージの管理
     public abstract bool handelMassage(Entitype enity, Telegram tele);
 }
