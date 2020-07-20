2d127f8f7ba562af6973100ff6384d05daf9c820
ShadowGameDeveloper <67132655+ShadowGameDeveloper@users.noreply.github.com>
2020-07-21 01:10:44 +0900

    Update GUIArray.cs

--- GUIArray.cs
+++ GUIArray.cs
@@ -12,20 +12,24 @@ public class GUIArray : MonoBehaviour
 
     void OnGUI()
     {
+        //array作ります
         int[] data = { 98, 72, 63, 89, 56 };
         int total = 0;
 
+        //data　array　の合計を計算
         foreach(int n in data)
         {
+
             total += n;
         }
-
+        //平均を計算
         int ave = total / 5;
         Message("合計 = "+total+" 平均 = "+ave);
     }
 
     void Message(string msg)
     {
+        //dataの平均はGUIに表示する
         GUI.Box(new Rect(10, 10, 350, 100), "Massage");
         GUI.Label(new Rect(35, 60, 300, 50), msg);
     }
