e60295f379971393a787560a226a1676f5b940b1
ShadowGameDeveloper <67132655+ShadowGameDeveloper@users.noreply.github.com>
2020-07-20 12:04:48 +0900

    Update BaseGameEntity.cs

--- Game AI/BaseGameEntity.cs
+++ Game AI/BaseGameEntity.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public abstract class BaseGameEntity : MonoBehaviour
 {
+    //動きスピードの設定
     public float moveSpeed = 0.01f;
-
+    //メッセージ
     public abstract void HandleMassage(Telegram tele);
 }
